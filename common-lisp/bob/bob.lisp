8f58ed86e99feefba3914cd50dfa3164c49282bf
Lukas Deutz <lukas.deutz@mailfence.com>
2018-09-30 11:21:16 +0200

    common-lisp/bob

--- common-lisp/bob/bob.lisp
+++ common-lisp/bob/bob.lisp
@@ -5,26 +5,27 @@
 (in-package #:bob)
 
 (defun response-for (input)
-  (let ((trimmed-input (string-trim
-                        (list #\Tab #\Newline #\Page #\Space) input)))
-    (let ((input_len (length trimmed-input)))
-      (labels ((upcase-p (x) (string= (string-upcase x) x))
-               (uppercase-and-alpha-p (x) (and
-                                           (upcase-p x)
-                                           (some #'alpha-char-p x)))
-               (input-last-char () (aref trimmed-input (1- input_len)))
-               (question-p () (char= #\? (input-last-char)))
-               (yelling-question-p () (and
-                                       (question-p)
-                                       (uppercase-and-alpha-p trimmed-input)))
-               (yelling-p () (and
-                              (char= #\! (input-last-char))
-                              (upcase-p trimmed-input)))
-               (yelling-statement-p () (uppercase-and-alpha-p trimmed-input)))
+  (let* ((trimmed-input (string-trim
+                         (list #\Tab #\Newline #\Page #\Space) input))
+         (input_len (length trimmed-input)))
+    (labels ((upcase-p (x) (string= (string-upcase x) x))
+             (uppercase-and-alpha-p (x) (and
+                                         (upcase-p x)
+                                         (some #'alpha-char-p x)))
+             (input-last-char () (aref trimmed-input (1- input_len)))
+             (silence-p () (= input_len 0))
+             (question-p () (char= #\? (input-last-char)))
+             (yelling-question-p () (and
+                                     (question-p)
+                                     (uppercase-and-alpha-p trimmed-input)))
+             (yelling-p () (and
+                            (char= #\! (input-last-char))
+                            (upcase-p trimmed-input)))
+             (yelling-statement-p () (uppercase-and-alpha-p trimmed-input)))
 
-        (cond ((= input_len 0) "Fine. Be that way!")
-              ((yelling-question-p) "Calm down, I know what I'm doing!")
-              ((question-p) "Sure.")
-              ((yelling-p) "Whoa, chill out!")
-              ((yelling-statement-p) "Whoa, chill out!")
-              (t "Whatever."))))))
+      (cond ((silence-p) "Fine. Be that way!")
+            ((yelling-question-p) "Calm down, I know what I'm doing!")
+            ((question-p) "Sure.")
+            ((yelling-p) "Whoa, chill out!")
+            ((yelling-statement-p) "Whoa, chill out!")
+            (t "Whatever.")))))
