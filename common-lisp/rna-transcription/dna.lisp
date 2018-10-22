ff830eb638e682680598d401fffd05d5e5a428c7
Lukas Deutz <lukas.deutz@mailfence.com>
2018-10-22 17:16:44 +0200

    added common-lisp/rna-transcription

---
+++ common-lisp/rna-transcription/dna.lisp
@@ -0,0 +1,20 @@
+(in-package #:cl-user)
+(defpackage #:dna
+  (:use #:cl)
+  (:export #:to-rna))
+(in-package #:dna)
+
+(defun to-rna (str)
+  (flet ((is-not-dna-p (dna-str)
+           (some #'(lambda (nucleotide)
+                     (char/= nucleotide #\G #\C #\T #\A))
+                 dna-str)))
+    (when (is-not-dna-p str) 'error)
+    (map 'string
+         #'(lambda (nucleotide)
+             (case nucleotide
+               (#\G #\C)
+               (#\C #\G)
+               (#\T #\A)
+               (#\A #\U)))
+         str)))
