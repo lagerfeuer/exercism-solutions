d91c218d63211995cf6688b7cebc343b7800a108
Lukas Deutz <lukas.deutz@mailfence.com>
2018-10-27 16:34:54 +0200

    prolog/sum_of_multiples: fix choicepoints/backtracking

--- prolog/sum-of-multiples/sum_of_multiples.pl
+++ prolog/sum-of-multiples/sum_of_multiples.pl
@@ -3,12 +3,12 @@ is_multiple([F|Factors], Number) :-
     is_multiple(Factors, Number).
 
 sum_up(Factors, Limit, Sum, NewSum) :-
-    is_multiple(Factors, Limit),
+    once(is_multiple(Factors, Limit)),
     NewSum is Limit + Sum;
     NewSum is Sum.
 
 sum_of_multiples(_, 0, 0).
 sum_of_multiples(Factors, Limit, Sum) :-
     NewLimit is Limit - 1,
-    sum_of_multiples(Factors, NewLimit, NewSum),
+    once(sum_of_multiples(Factors, NewLimit, NewSum)),
     sum_up(Factors, Limit, NewSum, Sum).
