edf61f533f583a1694bfa844d70da97710d933df
Lukas Deutz <lukas.deutz@mailfence.com>
2019-01-31 21:07:25 -0500

    c/binary-search

---
+++ c/binary-search/src/binary_search.c
@@ -0,0 +1,24 @@
+#include "binary_search.h"
+#include <stdlib.h>
+
+int* binary_search(int element, int* arr, unsigned int length) {
+  if (!arr || length == 0 || element < arr[0] || element > arr[length - 1])
+    return NULL;
+
+  int* start = arr;
+  int* end = arr + length;
+  int* middle;
+
+  while (start <= end) {
+    middle = start + (end - start) / 2;
+
+    if (element < *middle)
+      end = middle - 1;
+    else if (element > *middle)
+      start = middle + 1;
+    else
+      return middle;
+  }
+
+  return NULL;
+}
---
+++ c/binary-search/src/binary_search.h
@@ -0,0 +1,6 @@
+#ifndef BINARY_SEARCH_H
+#define BINARY_SEARCH_H
+
+int* binary_search(int element, int* arr, unsigned int length);
+
+#endif /* BINARY_SEARCH_H */
