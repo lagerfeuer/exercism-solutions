34af2cc9b0d4d2800bbdc8c7cfaaacb88107666c
Lukas Deutz <lukas.deutz@mailfence.com>
2019-02-08 21:46:59 -0500

    ruby/acronym

---
+++ ruby/acronym/acronym.rb
@@ -0,0 +1,10 @@
+class Acronym
+  def self.abbreviate(name)
+    name.tr(',-.', ' ').split.map{ |e| e[0] }.join.upcase
+  end
+end
+
+if __FILE__ == $0
+  puts "Usage: %{program_name} [Name] ..." % {:program_name => $0} if ARGV.empty?
+  ARGV.each{ |arg| puts Acronym.abbreviate(arg.to_s) }
+end
