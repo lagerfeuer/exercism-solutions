32167ca957c2c13d5f142bbe2e55fd7b14cd56d7
Lukas Deutz <lukas.deutz@mailfence.com>
2019-02-24 10:38:00 -0500

    c/allergies

---
+++ c/allergies/src/allergies.c
@@ -0,0 +1,25 @@
+#include "allergies.h"
+#include <stdlib.h>
+
+void get_allergens(int score, allergen_list_t* list) {
+  unsigned int allergen_score = 1;
+  int allergen_index = 0;
+  allergen_t* allergens =
+      malloc((ALLERGEN_CATS - ALLERGEN_EGGS) * sizeof(allergens));
+  if (!allergens)
+    return;
+  list->count = 0;
+  list->allergens = allergens;
+
+  while (allergen_score <= 128) {
+    if (score & allergen_score) {
+      list->allergens[list->count++] = allergen_index;
+    }
+    allergen_score <<= 1;
+    allergen_index++;
+  }
+}
+
+bool is_allergic_to(allergen_t allergen, int score) {
+  return ((1 << allergen) & score);
+}
