7388c1a16466b795e3052afef21da31150408c1a
Lukas Deutz <lukas.deutz@mailfence.com>
2019-02-24 10:38:12 -0500

    ruby/high-scores

---
+++ ruby/high-scores/high_scores.rb
@@ -0,0 +1,18 @@
+class HighScores
+  attr_reader :scores
+  def initialize(scores)
+    @scores = scores
+  end
+
+  def latest
+    @scores.last
+  end
+
+  def personal_best
+    @scores.sort{ |a,b| b <=> a }.first
+  end
+
+  def personal_top_three
+    @scores.sort{ |a,b| b <=> a }.take(3)
+  end
+end
