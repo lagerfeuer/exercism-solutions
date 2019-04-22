8c484a836ecd184dbcfc7a4b232959f0153a9694
Lukas Deutz <lukas.deutz@mailfence.com>
2019-04-22 22:04:51 +0200

    bash/triangle

--- bash/triangle/triangle.sh
+++ bash/triangle/triangle.sh
@@ -1,12 +1,10 @@
-function is_equilateral () {
-  if (( $1 == $2 && $2 == $3 && $1 > 0 )); then
-    return 0
-  else
-    return 1
-  fi
+#!/bin/bash
+
+is_equilateral () {
+  (( $1 == $2 && $2 == $3 && $1 > 0 ))
 }
 
-function is_isosceles () {
+is_isosceles () {
   if (( $1 == $2 || $1 == $3 || $2 == $3 )); then
     return 0
   else
@@ -14,33 +12,37 @@ function is_isosceles () {
   fi
 }
 
-function is_valid_triangle () {
+is_valid_triangle () {
   if (( $1 + $2 < $3 || $1 + $3 < $2 || $2 + $3 < $1 )); then
     return 1
   fi
   return 0
 }
 
-if ! is_valid_triangle "$2" "$3" "$4"; then
-  echo "false"
-  exit 1
-fi
+main() {
+  if ! is_valid_triangle "$2" "$3" "$4"; then
+    echo "false"
+    exit 1
+  fi
 
-case "$1" in
-  "equilateral")
-    is_equilateral "$2" "$3" "$4"
-    ;;
-  "isosceles")
-    is_isosceles "$2" "$3" "$4"
-    ;;
-  "scalene")
-    ! is_isosceles "$2" "$3" "$4"
-    ;;
-esac
+  case "$1" in
+    "equilateral")
+      is_equilateral "$2" "$3" "$4"
+      ;;
+    "isosceles")
+      is_isosceles "$2" "$3" "$4"
+      ;;
+    "scalene")
+      ! is_isosceles "$2" "$3" "$4"
+      ;;
+  esac
 
-if [[ "$?" -eq 0 ]]; then
-  echo "true"
-else
-  echo "false"
-  exit 1
-fi
+  if [[ "$?" -eq 0 ]]; then
+    echo "true"
+  else
+    echo "false"
+    exit 1
+  fi
+}
+
+main "$@"
