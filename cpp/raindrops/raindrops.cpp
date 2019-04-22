30097623d1e8a17df83d7aa955ac624b281be26a
Lukas Deutz <lukas.deutz@mailfence.com>
2019-04-22 22:17:16 +0200

    cpp/raindrops

--- cpp/raindrops/raindrops.cpp
+++ cpp/raindrops/raindrops.cpp
@@ -1,11 +1,16 @@
 #include "raindrops.h"
 #include <sstream>
+#include <vector>
+
+namespace raindrops {
+const std::vector<std::pair<unsigned int, std::string>> mapping = {
+    {3, "Pling"}, {5, "Plang"}, {7, "Plong"}};
+}
 
 std::string raindrops::convert(const unsigned int number) {
   std::stringstream result;
-  for (const auto kv : raindrops::mapping) {
+  for (const auto& kv : raindrops::mapping)
     if (number % kv.first == 0)
       result << kv.second;
-  }
   return (result.str().empty()) ? std::to_string(number) : result.str();
 }
--- cpp/raindrops/raindrops.h
+++ cpp/raindrops/raindrops.h
@@ -1,11 +1,7 @@
 #pragma once
 
-#include <map>
 #include <string>
 
 namespace raindrops {
-const std::map<unsigned int, std::string> mapping = {
-    {3, "Pling"}, {5, "Plang"}, {7, "Plong"}};
-
 std::string convert(const unsigned int number);
 } // namespace raindrops
