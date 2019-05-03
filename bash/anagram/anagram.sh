97515d96e0bd8e510303e5c2aba55cf08c45fa26
Lukas Deutz <lukas.deutz@mailfence.com>
2019-05-03 14:19:47 +0200

    bash/anagram

--- bash/anagram/anagram.sh
+++ bash/anagram/anagram.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
-input="${1,,}"
-declare -A letters
-for (( i = 0; i < ${#1}; i++ )); do
-  letter="${input:i:1}"
-  (( 'letters[$letter]'++ ))
-done
-
-result=""
-dict=("$2")
-for original_word in ${dict[@]}; do
-  word="${original_word,,}"
-  if [[ "$word" == "$input" ]]; then
-    continue
-  fi
-
-  declare -A used_letters
-  for (( i = 0; i < ${#word}; i++)); do
-    (( 'used_letters[${word:i:1}]'++ ))
+function main {
+  input="${1,,}"
+  declare -A letters
+  for (( i = 0; i < ${#1}; i++ )); do
+    letter="${input:i:1}"
+    (( 'letters[$letter]'++ ))
   done
 
-  found=1
-  for c in "${!letters[@]}"; do
-    if (( letters[$c] != used_letters[$c] )); then
-      found=0
+  result=()
+  dict=("$2")
+  for original_word in ${dict[@]}; do
+    word="${original_word,,}"
+    if [[ "$word" == "$input" ]]; then
+      continue
     fi
+
+    declare -A used_letters
+    for (( i = 0; i < ${#word}; i++)); do
+      (( 'used_letters[${word:i:1}]'++ ))
+    done
+
+    found=1
+    for c in "${!letters[@]}"; do
+      if (( letters[$c] != used_letters[$c] )); then
+        found=0
+      fi
+    done
+    if (( ${#letters[@]} != ${#used_letters[@]} )); then
+      for c in "${!used_letters[@]}"; do
+        if (( letters[$c] != used_letters[$c] )); then
+          found=0
+        fi
+      done
+    fi
+
+    if (( found == 1 )); then
+      result+=("$original_word")
+    fi
+
+    unset -v 'used_letters'
   done
-  for c in "${!used_letters[@]}"; do
-    if (( letters[$c] != used_letters[$c] )); then
-      found=0
-    fi
-  done
 
-  if (( found == 1 )); then
-    if [[ ! -z $result ]]; then
-      result+=" "
-    fi
-    result+="$original_word"
-  fi
+  echo "${result[@]}"
+}
 
-  unset -v 'used_letters'
-done
-
-echo "$result"
+main "$@"
