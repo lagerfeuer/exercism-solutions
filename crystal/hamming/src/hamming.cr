4eb056545dc62aa2becf3f4bbeac590343c02519
Lukas Deutz <lukas.deutz@mailfence.com>
2019-07-28 20:41:19 +0200

    improve crystal/hamming

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,13 +1,7 @@
 module Hamming
   def self.distance(a, b)
     raise ArgumentError.new("Sizes do not match") if a.size != b.size
-    distance = 0
-    a.chars.zip(b.chars).each do |ai,bi|
-      if ai != bi
-        distance += 1
-      end
-    end
-    return distance
+    return a.chars.zip(b.chars).count { |ai,bi| ai != bi }
   end
 end
 
