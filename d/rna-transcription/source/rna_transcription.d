f5e46dd7d8a6be9bf2a91f6e1727c25e9c2571bb
Lukas Deutz <lukas.deutz@mailfence.com>
2019-12-11 20:34:54 +0100

    fix d/rna

--- d/rna-transcription/source/rna_transcription.d
+++ d/rna-transcription/source/rna_transcription.d
@@ -1,17 +1,18 @@
 module rna_transcription;
 import std.string;
 
-
-
 string dnaComplement(const string dna) {
   const dchar[dchar] trans_table = ['G': 'C','C': 'G','T': 'A','A': 'U'];
+  const long idx = indexOfNeither(dna, trans_table.keys);
+  if(idx >= 0)
+    throw new Exception(format!"Found illegal char '%c'"(dna[idx]));
   return translate(dna, trans_table);
 }
 
 unittest {
   import std.exception : assertThrown;
 
-  const int allTestsEnabled = 0;
+  const int allTestsEnabled = 1;
 
   assert(dnaComplement("C") == "G");
   static if (allTestsEnabled) {
