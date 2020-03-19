0dc8e28456f473376fede52e88b3b3cfe6ee42b4
Lukas Deutz <lukas.deutz@mailfence.com>
2020-03-19 21:18:14 +0100

    solve python/book-store

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,4 +1,35 @@
-def calculate_total(books):
-    if not books:
-        return 0
+from copy import deepcopy
 
+_DISCOUNTS = [0, 0, .05, .1, .2, .25]
+_PRICE = 800
+
+
+def _cost(basket):
+    return _PRICE * (1.0 - _DISCOUNTS[len(basket)]) * len(basket)
+
+
+def cost(baskets):
+    return sum(_cost(basket) for basket in baskets)
+
+
+def calculate_total(books):
+    groups = []
+
+    # iterate over all books
+    for book in books:
+        cheapest = None
+        groups.append([])
+        # iterate over the groups so far
+        for idx, group in enumerate(groups):
+            # test the current book with the current group
+            # if not already in it
+            if book not in group:
+                new = deepcopy(groups)
+                new[idx].append(book)
+                # find the cheaper collection
+                cheapest = min(new, cheapest, key=cost) if cheapest else new
+
+        # clean the collection of empty groups
+        groups = [g for g in cheapest if g]
+
+    return cost(groups)
