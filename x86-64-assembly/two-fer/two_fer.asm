8a78290abd04b0e0aa99d957f0a218aa389309c7
Lukas Deutz <lukas.deutz@mailfence.com>
2020-03-20 21:15:22 +0100

    update asm/two-fer

--- x86-64-assembly/two-fer/two_fer.asm
+++ x86-64-assembly/two-fer/two_fer.asm
@@ -1,7 +1,8 @@
 ; asmsyntax=nasm
 section .rodata
-msg: db "One for ", 0x1A, ", one for me.", 0
-you: db "you", 0
+msg_start: db "One for ", 0
+msg_end: db ", one for me.", 0
+msg_you: db "you", 0
 
 section .text
 global two_fer
@@ -9,45 +10,50 @@ global two_fer
   ;     char* name   - rdi
   ;     char* output - rsi
 two_fer:
-  mov rdx, rdi  ; name buffer
-  mov rax, rsi  ; dst buffer
-  mov rbx, msg  ; message buffer
+  mov r8, rdi  ; name buffer
+  mov r9, rsi  ; dst buffer
 
-;;; strcat msg_start
-copy_loop:
-  mov cl, [rbx]       ; load msg[idx]
-  inc rbx             ; idx++
+; START
+  mov rdi, msg_start
+  mov rsi, r9
+  call strcat ; dst = strcat(msg_start, dst)
 
-  cmp cl, 0x1A        ; if msg[idx] == 0x1A
-  je subst            ; then SUBST
+; NAME
+name:
+  mov rdi, msg_you ; load 'you' // default
+  mov rsi, rax     ; returned from strcat before
+  test r8, r8      ; if name == NULL
+  jz .cont         ; then .cont
+  mov  rdi, r8     ; load *name
+.cont:
+  call strcat      ; dst = strcat(name, dst)
 
-  mov [rax], cl       ; dst[idx] = msg[idx]
-  inc rax             ; idx++
+; END
+  mov rdi, msg_end
+  mov rsi, rax
+  call strcat ; dst = strcat(msg_end, dst)
 
-  cmp cl, 0           ; if msg[idx] == NULL
-  je done             ; then DONE
-  jmp copy_loop       ; LOOP
+; RETURN
+  jmp return
 
+; char* strcat(char* src, char* dst)
+; returns (dst + strlen(src))
+strcat:
+  mov rax, rdi
+  mov rdx, rsi
+.loop:
+  mov cl, byte [rax]
+  mov byte [rdx], cl
+  inc rax
+  test cl, cl
+  jz .done
+  inc rdx
+  jmp .loop
+.done:
+  mov rax, rdx ; return last char of dst
+  ret
 
-subst:
-  cmp rdx, 0      ; if name == NULL
-  jne subst_loop
-  mov rdx, you    ; then rdx = "you"
-
-;;;;;;;; BUG ;;;;;;;;
-subst_loop:
-  mov cl, [rdx]   ; load name[idx]
-  cmp cl, 0       ; if name[idx] == NULL
-  je copy_loop    ; return
-
-  mov [rax], cl   ; dst[idx] = name[idx]
-  inc rax         ; idx++
-  inc rdx         ; idx++
-  jmp subst_loop  ; LOOP
-
-
-;;; set return value
-done:
-  mov rax, rsi
+return:
+  mov rax, r9  ; set return value
 
   ret
